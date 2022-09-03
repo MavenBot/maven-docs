c130d9070db1e4517f252b075d25b4dff6924520
Max <65555180+DiscordMax@users.noreply.github.com>
2022-09-02 21:16:18 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,3 +25,12 @@ function includeHTML() {
     }
   }
 };
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
